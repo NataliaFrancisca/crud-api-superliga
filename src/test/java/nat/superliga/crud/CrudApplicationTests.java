4fc9562e7d3b7bc0b27ab50bd4f83d6e9ead3971
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-01 23:56:39 -0300

    feat: initialize repository

---
+++ src/test/java/nat/superliga/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package nat.superliga.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
