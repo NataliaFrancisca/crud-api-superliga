5c114600dd608bebe4a2e70aeec5fcdef553bc5a
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-03 21:09:26 -0300

    feat: create coach repository

---
+++ src/main/java/nat/superliga/crud/domain/Coach/CoachRepository.java
@@ -0,0 +1,6 @@
+package nat.superliga.crud.domain.Coach;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoachRepository extends JpaRepository<Coach, String> {
+}
