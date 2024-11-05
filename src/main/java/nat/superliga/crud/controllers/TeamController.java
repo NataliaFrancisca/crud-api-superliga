f66cdbc5595593b381c37a8810f8540d9570dfab
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-04 23:12:58 -0300

    feat: create method PUT

--- src/main/java/nat/superliga/crud/controllers/TeamController.java
+++ src/main/java/nat/superliga/crud/controllers/TeamController.java
@@ -1,12 +1,16 @@
 package nat.superliga.crud.controllers;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import nat.superliga.crud.domain.Team.RequestTeam;
 import nat.superliga.crud.domain.Team.Team;
 import nat.superliga.crud.domain.Team.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/team")
@@ -26,5 +30,22 @@ public class TeamController {
         Team newTeam = new Team(data);
         repository.save(newTeam);
         return ResponseEntity.ok("Team created successfully");
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateTeam(@RequestBody @Valid RequestTeam data){
+        Optional<Team> optionalTeam = repository.findById(data.id());
+
+        if(optionalTeam.isEmpty()){
+            throw new EntityNotFoundException();
+        }
+
+        Team team = optionalTeam.get();
+        team.setName(data.name());
+        team.setState(data.state());
+        team.setCoach_id(data.coach_id());
+
+        return ResponseEntity.ok("Team updated successfully");
     }
 }
