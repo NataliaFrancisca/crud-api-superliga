6e4cf99736c797a68ec2fdc4ea501a0eca329c2f
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-05 21:58:27 -0300

    feat: set list of athletes

--- src/main/java/nat/superliga/crud/domain/Team/Team.java
+++ src/main/java/nat/superliga/crud/domain/Team/Team.java
@@ -2,6 +2,9 @@ package nat.superliga.crud.domain.Team;
 
 import jakarta.persistence.*;
 import lombok.*;
+import nat.superliga.crud.domain.Athlete.Athlete;
+
+import java.util.List;
 
 @Table(name="team")
 @Entity(name="team")
@@ -17,6 +20,9 @@ public class Team {
     private String name;
     private String coach_id;
     private String state;
+
+    @OneToMany(mappedBy = "team")
+    private List<Athlete> atheletes;
 
     public Team(RequestTeam requestTeam){
         this.name = requestTeam.name();
