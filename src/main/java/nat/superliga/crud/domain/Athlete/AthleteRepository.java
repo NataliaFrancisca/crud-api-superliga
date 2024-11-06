dfff0a203971c2b9d99524141c5d92348c596d7e
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-05 21:57:43 -0300

    feat: create athlete repository

---
+++ src/main/java/nat/superliga/crud/domain/Athlete/AthleteRepository.java
@@ -0,0 +1,6 @@
+package nat.superliga.crud.domain.Athlete;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AthleteRepository extends JpaRepository<Athlete, String> {
+}
