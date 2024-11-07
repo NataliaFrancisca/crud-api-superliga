22040d5d33be0276df8edf1bca872859ba478c44
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-06 23:34:52 -0300

    feat: create method POST and updated method GET to show data converted using DTO

--- src/main/java/nat/superliga/crud/controllers/AthleteController.java
+++ src/main/java/nat/superliga/crud/controllers/AthleteController.java
@@ -1,21 +1,39 @@
 package nat.superliga.crud.controllers;
 
-import nat.superliga.crud.domain.Athlete.AthleteRepository;
+import jakarta.validation.Valid;
+import nat.superliga.crud.domain.Athlete.*;
+import nat.superliga.crud.domain.Team.Team;
+import nat.superliga.crud.domain.Team.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/athlete")
 public class AthleteController {
+
     @Autowired
     private AthleteRepository repository;
+    @Autowired
+    private TeamRepository teamRepository;
 
     @GetMapping
     public ResponseEntity getAllAthletes(){
-        var allAthletes = repository.findAll();
-        return ResponseEntity.ok(allAthletes);
+        List<Athlete> athletes = repository.findAll();
+        List<AthleteDTO> athleteDTOS = athletes.stream().map(AthleteConverter::convertToDTO).toList();
+        return ResponseEntity.ok(athleteDTOS);
+    }
+
+    @PostMapping
+    public ResponseEntity setAthlete(@RequestBody @Valid RequestAthlete data){
+        Optional<Team> teamOptional = teamRepository.findById(data.team_id());
+
+        Athlete newAthlete = teamOptional.map(team -> new Athlete(data, team)).orElseGet(() -> new Athlete(data, null));
+        repository.save(newAthlete);
+
+        return ResponseEntity.ok("Athlete created successfully!");
     }
 }
