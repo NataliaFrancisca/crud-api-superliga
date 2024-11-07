9c972b6fb1d089559f2644cda605423a3acd9a9e
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-06 23:30:14 -0300

    feat: add id atribute and update age to birthdate

--- src/main/java/nat/superliga/crud/domain/Athlete/Athlete.java
+++ src/main/java/nat/superliga/crud/domain/Athlete/Athlete.java
@@ -1,8 +1,12 @@
 package nat.superliga.crud.domain.Athlete;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import nat.superliga.crud.domain.Team.Team;
+
+import java.time.LocalDate;
 
 @Table(name="athlete")
 @Entity(name="athlete")
@@ -14,21 +18,24 @@ import nat.superliga.crud.domain.Team.Team;
 public class Athlete {
     @Id @GeneratedValue(strategy = GenerationType.UUID)
 
+    private String id;
     private String name;
     private String position;
     private Double height;
-    private Integer age;
+    private LocalDate birthdate;
     private String nationality;
 
     @ManyToOne
     @JoinColumn(name = "team_id")
+    @JsonBackReference
     private Team team;
 
-    public Athlete(RequestAthlete requestAthlete){
+    public Athlete(RequestAthlete requestAthlete, Team team){
         this.name = requestAthlete.name();
         this.position = requestAthlete.position();
         this.height = requestAthlete.height();
-        this.age = requestAthlete.age();
+        this.birthdate = requestAthlete.birthdate();
         this.nationality = requestAthlete.nationality();
+        this.team = team;
     }
 }
