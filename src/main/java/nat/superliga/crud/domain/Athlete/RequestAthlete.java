d8a886a9bab6906b96940e240eb2c118a892c54f
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-06 23:30:46 -0300

    feat: change some validation rules

--- src/main/java/nat/superliga/crud/domain/Athlete/RequestAthlete.java
+++ src/main/java/nat/superliga/crud/domain/Athlete/RequestAthlete.java
@@ -1,19 +1,17 @@
 package nat.superliga.crud.domain.Athlete;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDate;
 
 public record RequestAthlete (
         String id,
-        @NotBlank
-        String name,
-        @NotBlank
-        String position,
-        @NotBlank
-        Double height,
-        @NotBlank
-        Integer age,
-        @NotBlank
-        String nationality,
-        @NotBlank
-        String team_id
+        @NotBlank String name,
+        @NotBlank String position,
+        @Positive Double height,
+        @NotNull LocalDate birthdate,
+        @NotBlank String nationality,
+        @NotBlank String team_id
 ){}
