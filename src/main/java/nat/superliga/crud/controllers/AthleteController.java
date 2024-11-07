c65b4ed1d75f2dfe1582a5d11c753fc4bf5a1198
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-06 23:59:30 -0300

    feat: create method PUT

--- src/main/java/nat/superliga/crud/controllers/AthleteController.java
+++ src/main/java/nat/superliga/crud/controllers/AthleteController.java
@@ -1,11 +1,13 @@
 package nat.superliga.crud.controllers;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import nat.superliga.crud.domain.Athlete.*;
 import nat.superliga.crud.domain.Team.Team;
 import nat.superliga.crud.domain.Team.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +22,10 @@ public class AthleteController {
     @Autowired
     private TeamRepository teamRepository;
 
+    private Optional<Team> getAthleteTeam(String team_id){
+        return teamRepository.findById(team_id);
+    }
+
     @GetMapping
     public ResponseEntity getAllAthletes(){
         List<Athlete> athletes = repository.findAll();
@@ -29,11 +35,40 @@ public class AthleteController {
 
     @PostMapping
     public ResponseEntity setAthlete(@RequestBody @Valid RequestAthlete data){
-        Optional<Team> teamOptional = teamRepository.findById(data.team_id());
+        var team = getAthleteTeam(data.team_id());
+        Athlete newAthlete = team.map(value -> new Athlete(data, value)).orElseGet(() -> new Athlete(data, null));
 
-        Athlete newAthlete = teamOptional.map(team -> new Athlete(data, team)).orElseGet(() -> new Athlete(data, null));
         repository.save(newAthlete);
-
         return ResponseEntity.ok("Athlete created successfully!");
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateAthlete(@RequestBody @Valid RequestAthlete data){
+        Optional<Athlete> athleteOptional = repository.findById(data.id());
+
+        if(athleteOptional.isEmpty()){
+            throw new EntityNotFoundException();
+        }
+
+        Athlete athlete = athleteOptional.get();
+
+        System.out.println(athlete);
+
+        athlete.setName(data.name());
+        athlete.setBirthdate(data.birthdate());
+        athlete.setPosition(data.position());
+        athlete.setHeight(data.height());
+        athlete.setNationality(data.nationality());
+
+        var team = getAthleteTeam(data.team_id());
+
+        if(team.isEmpty()){
+            athlete.setTeam(null);
+        }else{
+            athlete.setTeam(team.get());
+        }
+
+        return ResponseEntity.ok("Athlete updated successfully");
     }
 }
