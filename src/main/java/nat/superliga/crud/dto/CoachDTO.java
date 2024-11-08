b66b43b006b7e3a2b23cb65ec08ecfe2eba743e7
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-07 22:51:20 -0300

    feat: delete DTOs

--- src/main/java/nat/superliga/crud/dto/CoachDTO.java
+++
@@ -1,7 +0,0 @@
-package nat.superliga.crud.dto;
-
-public class CoachDTO {
-    private String id;
-    private String name;
-    private String nationality;
-}
--- src/main/java/nat/superliga/crud/dto/TeamDTO.java
+++
@@ -1,8 +0,0 @@
-package nat.superliga.crud.dto;
-
-public class TeamDTO {
-    private String id;
-    private String name;
-    private String state;
-    private String coach_id;
-}
